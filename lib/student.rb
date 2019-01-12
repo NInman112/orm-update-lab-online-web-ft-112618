d26f3d20bbfde61a0f682a35e4a890afa22636b6
Nick Inman <nick@theoatmeal.com>
2019-01-12 01:38:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,4 +37,18 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+
+  def self.create (name:, grade:)
+
+  end
+
+  def self.new_from_db
+
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn]execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
 end
