76df0bfd5781dfb981d9d64c105925716625cc0d
Nick Inman <nick@theoatmeal.com>
2019-01-12 01:31:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,14 +18,14 @@ class Student
           grade INTEGER
       )
       SQL
-      DB[:conn]execute(sql)
+      DB[:conn].execute(sql)
   end
 
   def self.drop_table
     sql = <<-SQL
       DROP TABLE IF EXISTS students
       SQL
-      DB[:conn]execute(sql)
+      DB[:conn].execute(sql)
   end
 
   def save
